9f9d1f3a7bdf1ebdd080d9c2f13bd90b4ed34818
Vladimir Chernenko <vet-family@yandex.ru>
2024-10-23 12:32:13 +0300

    change Main

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,24 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
+
         TaskManager taskManager = Managers.getDefault();
-        taskManager.createEpic("Privet", "Hello");
-        taskManager.createEpic("Buy some food", "I am gonna buy next food");
-        taskManager.createSubtask("Orange", "3 ripe orange", 2);
-        taskManager.createSubtask("Cucumbers", "2 cucumbers with pimples", 2);
-        taskManager.createSubtask("Apples", " 4 red apples", 2);
+        taskManager.createEpic("Встретиться с другом", "Литейный 21 в 16:00");
+        taskManager.createEpic("Купить еды", "Зайти в Магнит");
+        taskManager.createSubtask("Хлеб", "Черный", 2);
+        taskManager.createSubtask("Апельсины", "2 кг", 2);
+        taskManager.createSubtask("Мороженое", "Шоколадное и клубничное", 2);
 
         taskManager.getById(1);
         taskManager.getById(2);
-        taskManager.getById(3);
-//        taskManager.getById(4);
-//        taskManager.getById(5);
-//        taskManager.getById(2);
         System.out.println();
-        System.out.println(" Get History ");
+        System.out.println("Получить историю");
         taskManager.getHistory();
         System.out.println();
 
-        System.out.println("Deleted Epic\n");
         taskManager.removeById(2);
-        System.out.println(" Get History ");
+        System.out.println("Удален эпик под номером 2");
+        System.out.println("Получить историю");
         taskManager.getHistory();
     }
 }
